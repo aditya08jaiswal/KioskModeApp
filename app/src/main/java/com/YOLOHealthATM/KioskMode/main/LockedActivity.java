aaba32396ab6c1f91f26b1b0600b008ca909f6f1
iam844 <jaiswaladitya844@gmail.com>
2019-05-29 19:39:08 +0530

    Initial commit

---
+++ app/src/main/java/com/YOLOHealthATM/KioskMode/main/LockedActivity.java
@@ -0,0 +1,193 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.YOLOHealthATM.KioskMode.main;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityOptions;
+import android.app.admin.DevicePolicyManager;
+import android.app.admin.SystemUpdatePolicy;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class LockedActivity extends Activity {
+
+    private Button stopLockButton;
+    private ComponentName mAdminComponentName;
+    private DevicePolicyManager mDevicePolicyManager;
+    private PackageManager mPackageManager;
+
+    public static final String LOCK_ACTIVITY_KEY = "lock_activity";
+    public static final int FROM_LOCK_ACTIVITY = 1;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            super.onKeyDown(keyCode, event);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locked);
+
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+
+        // Setup stop lock task button
+        stopLockButton = findViewById(R.id.stop_lock_button);
+        stopLockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+                if (am.getLockTaskModeState() ==
+                            ActivityManager.LOCK_TASK_MODE_LOCKED) {
+                    stopLockTask();
+                }
+
+                setDefaultCosuPolicies(false);
+
+                Intent intent = new Intent(
+                        getApplicationContext(), MainActivity.class);
+
+                intent.putExtra(LOCK_ACTIVITY_KEY,FROM_LOCK_ACTIVITY);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        // Set Default COSU policy
+        mAdminComponentName = DeviceAdminReceiver.getComponentName(this);
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mPackageManager = getPackageManager();
+        if(mDevicePolicyManager.isDeviceOwnerApp(getPackageName())){
+            setDefaultCosuPolicies(true);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    R.string.not_device_owner,Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // start lock task mode if its not already active
+        if(mDevicePolicyManager.isLockTaskPermitted(this.getPackageName())){
+            ActivityManager am = (ActivityManager) getSystemService(
+                    Context.ACTIVITY_SERVICE);
+            if(am.getLockTaskModeState() ==
+                    ActivityManager.LOCK_TASK_MODE_NONE) {
+                startLockTask();
+            }
+        }
+    }
+
+
+    private void setDefaultCosuPolicies(boolean active){
+        // set user restrictions
+        setUserRestriction(UserManager.DISALLOW_SAFE_BOOT, active);
+        setUserRestriction(UserManager.DISALLOW_AIRPLANE_MODE, active);
+        setUserRestriction(UserManager.DISALLOW_FACTORY_RESET, active);
+        setUserRestriction(UserManager.DISALLOW_ADD_USER, active);
+        setUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA, active);
+        setUserRestriction(UserManager.DISALLOW_ADJUST_VOLUME, active);
+
+        // disable keyguard and status bar
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, active);
+        mDevicePolicyManager.setStatusBarDisabled(mAdminComponentName, active);
+
+        // enable STAY_ON_WHILE_PLUGGED_IN
+        enableStayOnWhilePluggedIn(active);
+
+        // set system update policy
+        if (active){
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName,
+                    SystemUpdatePolicy.createWindowedInstallPolicy(60, 120));
+        } else {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName,
+                    null);
+        }
+
+        // set this Activity as a lock task package
+
+        mDevicePolicyManager.setLockTaskPackages(mAdminComponentName,
+                active ? new String[]{getPackageName()} : new String[]{});
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_MAIN);
+        intentFilter.addCategory(Intent.CATEGORY_HOME);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        if (active) {
+            // set Cosu activity as home intent receiver so that it is started
+            // on reboot
+            mDevicePolicyManager.addPersistentPreferredActivity(
+                    mAdminComponentName, intentFilter, new ComponentName(
+                            getPackageName(), LockedActivity.class.getName()));
+        } else {
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(
+                    mAdminComponentName, getPackageName());
+        }
+    }
+
+    private void setUserRestriction(String restriction, boolean disallow){
+        if (disallow) {
+            mDevicePolicyManager.addUserRestriction(mAdminComponentName,
+                    restriction);
+        } else {
+            mDevicePolicyManager.clearUserRestriction(mAdminComponentName,
+                    restriction);
+        }
+    }
+
+    private void enableStayOnWhilePluggedIn(boolean enabled){
+        if (enabled) {
+            mDevicePolicyManager.setGlobalSetting(
+                    mAdminComponentName,
+                    Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+                    Integer.toString(BatteryManager.BATTERY_PLUGGED_AC
+                        | BatteryManager.BATTERY_PLUGGED_USB
+                        | BatteryManager.BATTERY_PLUGGED_WIRELESS));
+        } else {
+            mDevicePolicyManager.setGlobalSetting(
+                    mAdminComponentName,
+                    Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+                    "0"
+                    );
+        }
+    }
+
+}
---
+++ app/src/main/java/com/YOLOHealthATM/KioskMode/main/MainActivity.java
@@ -0,0 +1,67 @@
+package com.YOLOHealthATM.KioskMode.main;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityOptions;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class MainActivity extends Activity {
+
+    private Button lockTaskButton;
+    private PackageManager mPackageManager;
+
+    private DevicePolicyManager mDevicePolicyManager;
+    private ComponentName mAdminComponentName;
+
+    public static final String EXTRA_FILEPATH = "com.YOLOHealthATM.KioskMode.main.EXTRA_FILEPATH";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lockTaskButton = findViewById(R.id.start_lock_button);
+
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mAdminComponentName = DeviceAdminReceiver.getComponentName(this);
+        mPackageManager = this.getPackageManager();
+
+        if ( mDevicePolicyManager.isDeviceOwnerApp(getApplicationContext().getPackageName())) {
+
+            Intent lockIntent = new Intent(getApplicationContext(), LockedActivity.class);
+
+            mPackageManager.setComponentEnabledSetting(new ComponentName(getApplicationContext(), LockedActivity.class),
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+            startActivity(lockIntent);
+            finish();
+
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.not_lock_whitelisted,Toast.LENGTH_SHORT).show();
+        }
+
+        // Check to see if started by LockActivity and disable LockActivity if so
+
+        Intent intent = getIntent();
+
+        if(intent.getIntExtra(LockedActivity.LOCK_ACTIVITY_KEY,0) == LockedActivity.FROM_LOCK_ACTIVITY){
+
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(mAdminComponentName,getPackageName());
+
+            mPackageManager.setComponentEnabledSetting(new ComponentName(getApplicationContext(), LockedActivity.class),
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
+        }
+    }
+
+
+}
